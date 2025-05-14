0f6ea7f7feec4afdd12d7a13208b2f0af870e9b4
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-13 22:19:24 -0400

    Array de datos para prueba

---
+++ Data/DataArray.js
@@ -0,0 +1,14 @@
+export const DataArray = [
+    {
+        id: 1,
+        Title: 'Title 1'
+    },
+    {
+        id: 2,
+        Title: 'Title 2'
+    },
+    {
+        id: 3,
+        Title: 'Title 3'
+    }
+]
